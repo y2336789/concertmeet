e26665758eee09a87a39d0ffd3102e6120148886
y2336789 <you@example.com>
2021-08-13 17:29:59 +0900

    ..

--- concertmeet/settings.py
+++ concertmeet/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +139,5 @@ LOGOUT_REDIRECT_URL = '/'
 import dj_database_url 
 db_from_env = dj_database_url.config(conn_max_age=500) 
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
