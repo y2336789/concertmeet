f8e2ea58bbce30d9ba3982f80a2afe20d4ef9c44
y2336789 <you@example.com>
2021-08-13 17:20:57 +0900

    go

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn concertmeet.wsgi --log-file -
--- concertmeet/settings.py
+++ concertmeet/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dl!1@jk#)q6t8i99yyz$dgqge(8cxopgt^2y)f^v$8t3)kq77w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,3 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+import dj_database_url db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
+
+출처: https://egg-money.tistory.com/115 [완숙의 블로그]
